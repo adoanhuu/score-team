da52477b5f1b3bda82924c5e6d4efee3bca5113f
Alain DOAN-HUU <adoanhuu@figarocms.fr>
2026-03-03 08:03:42 +0100

    feat: update icons design

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ const ASSETS = [
   "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
-  "./icons/icon.svg",
+  "./icons/icon.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./icons/icon-maskable-512.png"
