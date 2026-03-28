e26b0c2253a305a439414459fe6655d8facb70e6
Alain DOAN-HUU <adoanhuu@figarocms.fr>
2026-03-28 21:14:39 +0100

    feat: update app version to v2.3.3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const ARROWS_PER_VOLLEY = 6;
-const APP_VERSION = "v2.3.2";
+const APP_VERSION = "v2.3.3";
 const LAST_SCORE_PREVIEW_MS = 300;
 const AUTO_SAVE_KEY = "score-team-autosave-v1";
 const HISTORY_KEY = "score-team-history-v1";
